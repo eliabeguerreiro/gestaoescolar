38331351a571cfa0bd6779cf08cdc367d31ee524
Eliabe <eliabepaz.wprk@gmail.com>
2020-06-29 23:25:32 -0300

    first commit

---
+++ conexao.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+function conectar(){
+	$servidor = "localhost:3308";
+	$usuario = "root";
+	$senha= "";
+	$bd = "gestaoescolar";
+	
+	$con = new mysqli($servidor, $usuario, $senha, $bd);
+	return $con;
+	
+}
+
+$conexao = conectar();
+
+
+?>
+
